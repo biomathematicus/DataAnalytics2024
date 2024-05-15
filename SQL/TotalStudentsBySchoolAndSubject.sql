2d3f2598972aeef2f959fe736f8785e21d9d78d5
khanhho5813 <khanhho5813@gmail.com>
2024-05-15 13:12:48 -0500

    Correct query for total subjects at all high schools in CRDB data

--- SQL/TotalStudentsBySchoolAndSubject.sql
+++ SQL/TotalStudentsBySchoolAndSubject.sql
@@ -1,22 +1,12 @@
-/*
-This query returns the total number of students by high school, and each subject area
-*/
-
-select E.lea_state, E.leaid, E.lea_name, E.schid, E.sch_name,  
-		cast(tot_enr_m as integer) + cast(tot_enr_f as integer) as TOTAL,
-		cast(tot_mathenr_advm_m as integer) + cast(tot_mathenr_advm_f as integer) as ADVMATH
-from 	"CRDB".schoolcharacteristics SC
-		join "CRDB".enrollment E 
-		on SC.schid = E.schid and SC.leaid = E.leaid
-		join "CRDB".advancedmathematics AM
-		on AM.schid = E.schid and AM.leaid = E.leaid
-where	sch_grade_g09 = 'Yes'
-		and sch_grade_g10  = 'Yes'
-		and sch_grade_g11 = 'Yes'
-		and sch_grade_g12 = 'Yes'
-		and cast(tot_enr_m as integer) > 0
-		and cast(tot_enr_f as integer) > 0
-		and cast(tot_mathenr_advm_m as integer) > 0
-		and cast(tot_mathenr_advm_f as integer) > 0
-order by E.lea_state, E.lea_name, E.sch_name
-limit 100;
+SELECT LEA_STATE, LEAID, LEA_NAME, SCHID, SCH_NAME, TOTAL, 
+       CASE WHEN APMath < 0 THEN NULL ELSE APMath END AS APMath,
+	   CASE WHEN AdvMath < 0 THEN NULL ELSE AdvMath END AS AdvMath,
+	   CASE WHEN APScience < 0 THEN NULL ELSE APScience END AS APScience,
+	   CASE WHEN APOthers < 0 THEN NULL ELSE APOthers END AS APOthers,
+       CASE WHEN AlgII < 0 THEN NULL ELSE AlgII END AS AlgII,
+	   CASE WHEN Bio < 0 THEN NULL ELSE Bio END AS Bio,
+	   CASE WHEN Cal < 0 THEN NULL ELSE Cal END AS Cal,
+	   CASE WHEN Chem < 0 THEN NULL ELSE Chem END AS Chem,
+	   CASE WHEN Geo < 0 THEN NULL ELSE Geo END AS Geo,
+	   CASE WHEN Phy < 0 THEN NULL ELSE Phy END AS Phy
+FROM HSTOTALS();
